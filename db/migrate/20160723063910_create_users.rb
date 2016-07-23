e7394b666fb1942c714512b66c1851431b390821
Patrick Ofilada <pcofilada@gmail.com>
2016-07-23 20:29:08 +0800

    Add mobile number authentication and confirmation

---
+++ db/migrate/20160723063910_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :mobile_number
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :gender
+      t.datetime :birth_date
+
+      t.timestamps
+    end
+  end
+end
---
+++ lib/payment.rb
@@ -0,0 +1,45 @@
+class Payment
+  # CONSTANTS
+  URL = 'http://devapi.globelabs.com.ph/payment/%s/transactions/amount'
+
+  # CLASS VARIABLES
+  @@accessToken  = String.new
+  @@subscriber   = String.new
+  @@version      = 'v1'
+
+  # Initialize Access Token
+  def initialize(accessToken, subscriber)
+    @@accessToken = accessToken
+    @@subscriber  = subscriber
+  end
+
+  # Sends a request using POST method
+  def charge(amount, referenceCode)
+    url = sprintf(URL, @@version)
+
+    #Request as POST METHOD
+    uri      = URI.parse(url)
+    http     = Net::HTTP.new(uri.host, uri.port)
+    request  = Net::HTTP::Post.new(url)
+
+    params = {
+      'transactionOperationStatus' => 'charged',
+      'access_token'               => @@accessToken,
+      'endUserId'                  => @@subscriber,
+      'amount'                     => amount,
+      'referenceCode'              => referenceCode
+    }
+
+    request.set_form_data(params)
+
+    response = http.request(request)
+
+    return response.body
+  end
+
+  # Set the API Version
+  def setVersion(version)
+    @@version = version
+    return self
+  end
+end
